4e82cfedde81872f54f7d10430b44e9c386ada52
softopia <softopia.tk@gmail.com>
2023-02-04 18:35:13 +0530

    added vite config

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig, loadEnv } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
+import fs from "node:fs";
+export default defineConfig(function ({ mode }) {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+    return {
+        plugins: [
+            laravel(["resources/css/app.css", "resources/js/app.js"]),
+            react(),
+        ],
+        define: {
+            "process.env.SHOPIFY_API_KEY": JSON.stringify(
+                process.env.VITE_SHOPIFY_API_KEY
+            ),
+        },
+        resolve: {
+            preserveSymlinks: true,
+        },
+        server: {
+            host: "localhost",
+            https: {
+                key: fs.readFileSync("../../etc/ssl/laragon.key"),
+                cert: fs.readFileSync("../../etc/ssl/laragon.crt"),
+            },
+        },
+    };
+});
